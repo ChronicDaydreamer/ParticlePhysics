641b3832db9cefd33dc6439c8d48627209b2cc3e
Micah Thomas <thoma223@wwu.edu>
2025-02-04 20:21:40 -0800

    reorganized

--- Particle.java
+++ Particle.java
@@ -1,8 +1,17 @@
+import java.awt.Color;
+import java.awt.Graphics;
 public class Particle {
     public int x;
     public int y;
+    
     public Particle(int x,int y){
         this.x=x;
         this.y=y;
+    }
+
+    public void draw(Graphics g){
+        g.setColor(Color.red);
+        System.out.println(this.y);
+        g.fillOval(this.x, this.y, 5, 5);
     }
 }
